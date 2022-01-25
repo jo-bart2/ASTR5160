fc0d9f68e7cd32cb130db0b2727c56cdde5314cf
Jordan Bartlett <jbartle9@uwyo.edu>
2022-01-25 09:00:53 -0700

    plotted general RA vs DEC

--- week2/day1.py
+++ week2/day1.py
@@ -2,3 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 from astropy.table import Table
 
+#JAB Read in the fits file
+objs = Table.read("/d/scratch/ASTR5160/week2/struc.fits")
+
+#JAB Determine indeces of objects whose extinction is greater than 0.2
+
+
+#JAB Plot RA vs DEC and Overplot specific objects
+plt.plot(objs["RA"],objs["DEC"],'o',label='All RA and DEC')
+plt.xlabel('RA')
+plt.ylabel('DEC')
+plt.show()
