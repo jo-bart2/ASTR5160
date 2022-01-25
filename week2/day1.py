62c6df5cb06172819437ec917f801a4e3eeb870d
Jordan Bartlett <jbartle9@uwyo.edu>
2022-01-25 09:16:25 -0700

    plotted all points together

--- week2/day1.py
+++ week2/day1.py
@@ -6,11 +6,13 @@ from astropy.table import Table
 objs = Table.read("/d/scratch/ASTR5160/week2/struc.fits")
 
 #JAB Determine lists of RA and DEC whose extinction is greater than 0.2
-new_ra = objs["RA"][objs["EXTINCTION"][:,0] >=0.2]
-new_dec = objs["DEC"][objs["EXTINCTION"][:,0] >=0.2]
+new_ra = objs["RA"][objs["EXTINCTION"][:,0] >0.2]
+new_dec = objs["DEC"][objs["EXTINCTION"][:,0] >0.2]
 
 #JAB Plot RA vs DEC and Overplot specific objects
-plt.plot(objs["RA"],objs["DEC"],'o',label='All RA and DEC')
+plt.plot(objs["RA"],objs["DEC"],'o',alpha=0.5,label='All RA and DEC')
+plt.plot(new_ra,new_dec,'x',color='orange',label='Extinction > 0.2')
 plt.xlabel('RA')
 plt.ylabel('DEC')
+plt.legend()
 plt.show()
