f437a4f83339e726e69b403cc1ce750ed93b9ea0
Jordan Bartlett <jbartle9@uwyo.edu>
2022-01-25 09:21:56 -0700

    started extra problem

--- week2/day1.py
+++ week2/day1.py
@@ -11,8 +11,10 @@ new_dec = objs["DEC"][objs["EXTINCTION"][:,0] >0.2]
 
 #JAB Plot RA vs DEC and Overplot specific objects
 plt.plot(objs["RA"],objs["DEC"],'o',alpha=0.5,label='All RA and DEC')
-plt.plot(new_ra,new_dec,'x',color='orange',label='Extinction > 0.2')
+plt.plot(new_ra,new_dec,'x',color='red',label='Extinction > 0.2')
 plt.xlabel('RA')
 plt.ylabel('DEC')
 plt.legend()
 plt.show()
+
+#JAB Create 3-array of random numbers
