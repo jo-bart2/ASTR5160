64aed0ceee52232334bdc797d29a034c2f3f53e3
Jordan Bartlett <jbartle9@uwyo.edu>
2022-01-25 09:12:35 -0700

    found ra and dec of specific extinctions

--- week2/day1.py
+++ week2/day1.py
@@ -5,8 +5,9 @@ from astropy.table import Table
 #JAB Read in the fits file
 objs = Table.read("/d/scratch/ASTR5160/week2/struc.fits")
 
-#JAB Determine indeces of objects whose extinction is greater than 0.2
-
+#JAB Determine lists of RA and DEC whose extinction is greater than 0.2
+new_ra = objs["RA"][objs["EXTINCTION"][:,0] >=0.2]
+new_dec = objs["DEC"][objs["EXTINCTION"][:,0] >=0.2]
 
 #JAB Plot RA vs DEC and Overplot specific objects
 plt.plot(objs["RA"],objs["DEC"],'o',label='All RA and DEC')
