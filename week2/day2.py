142ce7d56fe43bd0528461d7a133f3beaaf0c6d7
Jordan Bartlett <jbartle9@uwyo.edu>
2022-01-27 08:41:41 -0700

    convert ra and dec to degrees

--- week2/day2.py
+++ week2/day2.py
@@ -1,1 +1,12 @@
 import numpy as np
+from astropy.coordinates import SkyCoord
+from astropy.time import Time
+
+#JAB RA and Dec of the star Procyon
+r = '07h39m18s' #RA
+d = '+05d13m30s' #Dec
+
+#JAB Convert RA and Dec to degrees
+deg = SkyCoord(r,d,frame='icrs')
+print(deg)
+print('The RA and Dec of Procyon in degrees is ' + str(deg.ra.degree) + ', ' + str(deg.dec.degree))
