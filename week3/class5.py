30f2bfc6ad0e457ca9dea703a2b70e8b6a160676
Jordan Bartlett <jbartle9@uwyo.edu>
2022-02-03 15:29:16 -0700

    convert to cartesian

--- week3/class5.py
+++ week3/class5.py
@@ -1,3 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy.coordinates import SkyCoord
+
+#JAB RA and Dec of the star Procyon
+r = '07h39m18s' #RA
+d = '+05d13m30s' #Dec
+
+#JAB Convert RA and Dec to cartesian with astropy
+coords = SkyCoord(r,d,frame='icrs')
+coords.representation_type = 'cartesian'
+print('The coordinates of Procyon in cartesian are:')
+print('x = ' + str(coords.x))
+print('y = ' + str(coords.y))
+print('z = ' + str(coords.z))
