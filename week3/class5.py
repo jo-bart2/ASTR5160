e0986b2b53093fce2f35bfd234fbe6839462b002
Jordan Bartlett <jbartle9@uwyo.edu>
2022-02-03 16:16:18 -0700

    working on matching function

--- week3/class5.py
+++ week3/class5.py
@@ -13,3 +13,31 @@ print('The coordinates of Procyon in cartesian are:')
 print('x = ' + str(coords.x))
 print('y = ' + str(coords.y))
 print('z = ' + str(coords.z))
+
+#JAB Check SkyCoord matches given equations
+def convert(Ra,Dec):
+    coor = SkyCoord(Ra,Dec,frame='icrs')
+    print(coor.dec.degree)
+    new_x = float(np.cos(coor.ra.degree)*np.cos(coor.dec.degree))
+    new_y = float(np.sin(coor.ra.degree)*np.cos(coor.dec.degree))
+    new_z = float(np.sin(coor.dec.degree))
+    
+    return new_x,new_y,new_z
+
+def match(Ra,Dec,x,y,z):
+    x = float(x)
+    y = float(y)
+    z = float(z)
+    
+    x2,y2,z2 = convert(Ra,Dec)
+    
+    if round(x2,4) == round(x,4) and round(y2,4) == round(y,4) and round(z2,4) == round(z,4):
+        print('SkyCoord output matches given equations')
+        print(x,y,z)
+        print(x2,y2,z2 )
+    else:
+        print('SkyCoord output does not match given equations')
+        print(x,y,z)
+        print(x2,y2,z2 )
+        
+match(r,d,coords.x,coords.y,coords.z)
