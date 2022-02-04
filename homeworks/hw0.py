835fae85371bd7f2d0d2b6023407d60b31f0df5b
Jordan Bartlett <jbartle9@uwyo.edu>
2022-02-04 16:22:18 -0700

    put it all together

--- homeworks/hw0.py
+++ homeworks/hw0.py
@@ -49,9 +49,26 @@ plt.errorbar(x,y,y_err,color='black',label='Data',fmt='o')
 plt.plot(x,linear(x,2,1),color='purple',linestyle='dashed',label='Original Line',linewidth=0.5)
 plt.plot(x,linear(x,popt[0],popt[1]),color='green',linestyle='dashed',label='Best Fit',linewidth=0.5)
 plt.legend()
+plt.savefig('/d/users/jordan/Desktop/astro_tech_II/hw0.png') #JAB Problem 4
+plt.show()
 
-#JAB Problem 4: 
+def AllTogether():
+    m = input('m = ')
+    b = input('b = ')
+    
+    x, y, y_err = CreateArrays(m,b)
+    
+    popt, pcov = curve_fit(linear,x,y,sigma=y_err)
+    print('The recovered values are: m = '+str(popt[0])+', b = '+str(popt[1]))
 
-
+    plt.errorbar(x,y,y_err,color='black',label='Data',fmt='o')
+    plt.plot(x,linear(x,2,1),color='purple',linestyle='dashed',label='Original Line',linewidth=0.5)
+    plt.plot(x,linear(x,popt[0],popt[1]),color='green',linestyle='dashed',label='Best Fit',linewidth=0.5)
+    plt.legend()
+    plt.savefig('/d/users/jordan/Desktop/astro_tech_II/hw0.png')
+    plt.show()
+    
+if __name__ == '__main__':
+    AllTogether()
 
 
