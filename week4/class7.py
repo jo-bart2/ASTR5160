64e1aa29a062f424df7002acdb140955ab3383f7
Jordan Bartlett <jbartle9@uwyo.edu>
2022-02-11 10:50:25 -0700

    part 1 and start of part 2

--- week4/class7.py
+++ week4/class7.py
@@ -1,3 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy.random import random
+
+# JAB create random set of 10,000 points for ra and dec coordinates in radians
+ra = 2*np.pi*(random(10000)-0.5)
+dec = np.arcsin(1.-random(10000)*2.)
+
+# JAB plot points on (x, y) grid
+plt.scatter(ra, dec, marker='o', color='purple', alpha=0.5)
+plt.xlabel('RA (radians)')
+plt.ylabel('Dec (radians)')
+plt.show()
+# JAB In this projection, there are more points along the equator, near Dec = 0, than there are near the poles (dec = +/- pi/2)
+
+# JAB Plot the same points in an Aitoff projection
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='aitoff')
+ax.scatter(ra, dec, marker='o', color='goldenrod', s=0.7, alpha=0.5)
+xlab = ['14h','16h','18h','20h','22h','0h','2h','4h','6h','8h','10h']
+ax.set_xticklabels(xlab, weight=800)
+ax.grid(color='b', linestyle='dashed', linewidth=1.5)
+fig.show()
+
 
