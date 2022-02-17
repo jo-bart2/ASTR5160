77e0f835953511e82371c242873bedf4d3e93c38
Jordan Bartlett <jbartle9@uwyo.edu>
2022-02-17 16:32:52 -0700

    part of first function

--- week5/class10.py
+++ week5/class10.py
@@ -1,1 +1,17 @@
 import numpy as np
+from astropy.coordinates import SkyCoord
+
+# JAB Problem 1
+# JAB Write a function for a spherical cap bounded by ra=5h
+def ra_cap(ra):
+    dec = '0d'
+    h = 1
+    
+    c = SkyCoord(ra, dec, frame='icrs')
+    c.representation_type = 'cartesian'
+
+    cap = np.array([c.x.value, c.y.value, c.z.value, h])
+    
+    return cap
+
+print(ra_cap('5h'))
