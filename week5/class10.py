63a3e7f5be06c8be937e457a5532f0f8a71032cc
Jordan Bartlett <jbartle9@uwyo.edu>
2022-02-18 15:51:10 -0700

    found array for ra bound

--- week5/class10.py
+++ week5/class10.py
@@ -1,17 +1,20 @@
 import numpy as np
 from astropy.coordinates import SkyCoord
+import astropy.units as u
 
 # JAB Problem 1
 # JAB Write a function for a spherical cap bounded by ra=5h
 def ra_cap(ra):
     dec = '0d'
-    h = 1
+    h1 = 1
     
     c = SkyCoord(ra, dec, frame='icrs')
+    ra_new, dec_new = c.ra.degree+90, c.dec.degree
+    c = SkyCoord(ra_new, dec, frame='icrs', unit='deg')
     c.representation_type = 'cartesian'
 
-    cap = np.array([c.x.value, c.y.value, c.z.value, h])
+    cap = np.array([c.x.value, c.y.value, c.z.value, h1])
     
     return cap
 
-print(ra_cap('5h'))
+print('The 4-array for the cap bounded by 5h in RA is: '+str(ra_cap('5h')))
