b7f34852fa7ae65eebf8794be52a5647d9cec487
Jordan Bartlett <jbartle9@uwyo.edu>
2022-02-25 14:40:10 -0700

    start function

--- homeworks/hw2.py
+++ homeworks/hw2.py
@@ -1,2 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+def field_area(ra_min, ra_max, dec_min, dec_max):
+    '''
+    Parameters
+    ----------
+
+    Returns
+    -------
+
+    '''
+    
