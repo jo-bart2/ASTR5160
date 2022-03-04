790317aaf263060db2c112d4d6fb56b999633e99
Jordan Bartlett <jbartle9@uwyo.edu>
2022-03-04 15:36:33 -0700

    plot rectangles and fill between

--- homeworks/hw2.py
+++ homeworks/hw2.py
@@ -28,16 +28,15 @@ def field_area(r_min, r_max, d_min, d_max):
 
     return area
 
-def plot_rect(coords):
+def plot_rect(coords, plot_dir):
     '''
     Parameters:
     -----------
     coords: :class: '~numpy.ndarray'
           An array of arrays containing ra and dec boundaries of each rectangle
           in the form [ra_min. ra_max, dec_min, dec_max]
-
-    Returns:
-    --------
+    dir: :class: 'string'
+       The string representing the directory in which to save the plot
     '''
 
     # JAB Determine the number of rectangles to plot
@@ -47,22 +46,29 @@ def plot_rect(coords):
     area = [field_area(i[0], i[1], i[2], i[3]) for i in coords]
     
     # JAB Convert ra and dec to radians
-    c_rad = [[np.deg2rad(i[0]), np.deg2rad(i[1]), np.deg2rad(i[2]), np.deg2rad(i[3])] for i in coords]
+    c_rad = [[np.deg2rad(i[0]), np.deg2rad(i[1]), np.deg2rad(i[2]), 
+              np.deg2rad(i[3])] for i in coords]
     
     # JAB Determine lines to be plotted
     l_r_min = [[np.linspace(i[0], i[0]), np.linspace(i[2], i[3])] for i in c_rad]
     l_r_max = [[np.linspace(i[1], i[1]), np.linspace(i[2], i[3])] for i in c_rad]
     l_d_min = [[np.linspace(i[0], i[1]), np.linspace(i[2], i[2])] for i in c_rad]
     l_d_max = [[np.linspace(i[0], i[1]), np.linspace(i[3], i[3])] for i in c_rad]
+    # JAB Make list of colors for plots
+    clrs = ['r','g','c','m', 'y', 'purple', 'orange']
     # JAB Plot the rectangles on the sphere
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='aitoff')
     for i in range(len(l_r_min)):
-        ax.plot(l_r_min[i][0], l_r_min[i][1], color='purple')
-        ax.plot(l_r_max[i][0], l_r_max[i][1], color='purple')
-        ax.plot(l_d_min[i][0], l_d_min[i][1], color='purple')
-        ax.plot(l_d_max[i][0], l_d_max[i][1], color='purple')
+        ax.plot(l_r_min[i][0], l_r_min[i][1], color=clrs[i])
+        ax.plot(l_r_max[i][0], l_r_max[i][1], color=clrs[i])
+        ax.plot(l_d_min[i][0], l_d_min[i][1], color=clrs[i])
+        ax.plot(l_d_max[i][0], l_d_max[i][1], color=clrs[i])
+        ax.fill_between(l_d_min[i][0], l_d_min[i][1], l_d_max[i][1], color=clrs[i], 
+                        label='Area = {} sqr deg'.format(round(area[i],2)), alpha=0.5)
     ax.grid(color='k', linestyle='solid', linewidth=0.6)
+    ax.legend(loc='lower left')
+    plt.savefig(plot_dir)
     plt.show()
 
 
@@ -82,4 +88,5 @@ if __name__ == '__main__':
     a = field_area(0,180,0,90)
     print('The area of a rectangle bounded by (0, 360, 0, 90) is: {}'.format(a))
 
-    plot_rect([[0,45,0,45], [-90,-45,0,45]])
+    # JAB Create list of rectangle coordinates and plot
+    
