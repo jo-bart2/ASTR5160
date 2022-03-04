cd0b168ad819ef792fedb411788679a3ccd01127
jo-bart2 <jbartle9@uwyo.edu>
2022-03-04 12:49:51 -0700

    working on it

--- homeworks/hw2.py
+++ homeworks/hw2.py
@@ -24,7 +24,7 @@ def field_area(r_min, r_max, d_min, d_max):
     
     # JAB Determine the area of the lat-lon rectangle
     z1, z2 = np.sin(np.deg2rad(d_min)), np.sin(np.deg2rad(d_max))
-    area = (r_max-r_min)*(z2-z1)
+    area = (r_max-r_min)*(z2-z1)*(180/np.pi)
 
     return area
 
@@ -41,15 +41,21 @@ def plot_rect(coords):
     '''
     Parameters:
     -----------
+    coords: :class: '~numpy.ndarray'
+          An array of arrays containing ra and dec boundaries of each rectangle
+
     Returns:
     --------
     '''
     
     # JAB Determine the number of rectangles to plot
-    num = 
+    num = len(coords)
+
+    # JAB 
+    
 
 if __name__ == '__main__':
     
     # JAB Check function returns the correct value for (0,360,0.90)
-    a = field_area(0,360,0,90)
+    a = field_area(0,180,0,90)
     print('The area of a rectangle bounded by (0, 360, 0, 90) is: {}'.format(a))
