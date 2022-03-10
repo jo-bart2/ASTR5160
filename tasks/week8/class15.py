dfb3533e9492602d6309c976563e5adb03d2e167
Jordan Bartlett <jbartle9@uwyo.edu>
2022-03-10 15:05:55 -0700

    first plot

--- tasks/week8/class15.py
+++ tasks/week8/class15.py
@@ -1,4 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 # JAB Problem 2
+# JAB Read in csv file
+data = pd.read_csv('data.csv', delimiter=',')
+
+# JAB Plot ra vs dec
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(data['ra'], data['dec'], marker='o', c='forestgreen')
+ax.set_xlabel('RA (deg)'), ax.set_ylabel('Dec (deg)')
+plt.show()
+
+# JAB Replot with different sized points
