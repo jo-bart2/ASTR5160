799cfc5ecffa56acf99b6f82310c15b2d5eeca00
Jordan Bartlett <jbartle9@uwyo.edu>
2022-03-10 15:53:02 -0700

    plot first points and add new file

--- tasks/week8/class16.py
+++ tasks/week8/class16.py
@@ -2,3 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from astropy.table import Table
 
+# JAB Problem 1
+file_dir = '/d/scratch/ASTR5160/data/first/first_08jul16.fits'
+data = Table.read(file_dir)
+#print(data)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(data['RA'], data['DEC'], color='forestgreen', s=0.1)
+ax.set_xlabel('RA (deg)'), ax.set_ylabel('Dec (deg)')
+plt.show()
