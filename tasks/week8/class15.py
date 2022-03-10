c3aad42e7945a81b892f952f8f4fca73f64a6091
Jordan Bartlett <jbartle9@uwyo.edu>
2022-03-10 15:27:07 -0700

    replot with varying sizes

--- tasks/week8/class15.py
+++ tasks/week8/class15.py
@@ -14,3 +14,10 @@ ax.set_xlabel('RA (deg)'), ax.set_ylabel('Dec (deg)')
 plt.show()
 
 # JAB Replot with different sized points
+area = ((1/data['g'])*200)**2
+
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(111)
+ax1.scatter(data['ra'], data['dec'], marker='o', c='forestgreen', s=area, alpha=0.6)
+ax1.set_xlabel('RA (deg)'), ax1.set_ylabel('Dec (deg)')
+plt.show()
