b6d9fb399cc933aa831a87ede994266df5b93d49
jo-bart2 <jbartle9@uwyo.edu>
2022-03-22 10:27:52 -0600

    compare transformations to sdss magnitudes

--- tasks/week10/class17.py
+++ tasks/week10/class17.py
@@ -2,3 +2,24 @@ import numpy as np
 
 # JAB Problem 1
 # JAB Use UBVRI to ugriz transformations for g and z magnitudes
+# JAB From provided info, the magnitudes of the star are:
+v = 15.256 # V magnitude
+bv = 0.873 # B-V magnitude
+ri = 0.511 # R-I magnitude
+
+# JAB Use Jester et al. (2005) transformations
+r = v - 0.42*bv + 0.11
+rz = 1.72*ri - 0.41
+
+z = r - rz
+g = v + 0.60*bv - 0.12
+
+# JAB Compare magnitudes to those in SDSS Navigator
+# JAB Star coordinates: (16:35:26, +09:47:53) = (248.8583, 9.7981)
+g_nav = 15.70
+z_nav = 14.55
+
+print('The g magnitude is expected to be {} and is {} in SDSS'.format(g, g_nav))
+print('The z magnitude is expected to be {} and is {} in SDSS'.format(z, z_nav))
+
+
