82793b7891e3834198a523af616f3a849122981f
Jordan Bartlett <jbartle9@uwyo.edu>
2022-03-25 11:24:12 -0600

    plotting and determining stars and qsos

--- tasks/week10/class18.py
+++ tasks/week10/class18.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from astropy.table import Table
 from astropy.coordinates import SkyCoord
 import astropy.units as u
+import argparse
 from tasks.week8.class16 import coords_from_sweep, sweep_files    
 
 # JAB Function to convert fluxes to magnitudes
@@ -22,11 +23,59 @@ def flux_to_mag(fluxes):
     
     return mags
 
+# JAB Function for classifying line
+def starqso_line(color):
+    '''
+    Parameters
+    ----------
+    color: :class: '~numpy.ndarray'
+       An array of the g-z magnitudes to be input into the line
+
+    Returns
+    -------
+    line: :class: '~numpy.ndarray'
+       An array of the resulting r-W1 magnitudes from the line
+    '''
+    line = 1.0*color - 1.0
+
+    return line
+
 # JAB Function to determine star or quasar based on cut
-def classify_objs():
+def classify_objs(xcolor, ycolor):
+    '''
+    Parameters
+    ----------
+    xcolor: :class: '~numpy.ndarray'
+       An array of the color magnitudes on the x axis
+
+    ycolor: :class: '~numpy.ndarray'
+       An array of the color mangitudes on the y axis
+
+    Returns
+    -------
+    stars: :class: '~numpy.ndarray'
+       An array containing the xcolor and ycolor values for the stars
+
+    qsos: :class: '~numpy.ndarray'
+       An array containing the xcolor and ycolor valyes for the qsos
+    '''
+    # JAB Use the equation for the dividing line to distinguish
+    line = starqso_line(xcolor)
     
+    # JAB Determine which objects are above or below the line
+    stars = np.array([xcolor[ycolor <= line], ycolor[ycolor <= line]])
+    qsos = np.array([xcolor[ycolor > line], ycolor[ycolor > line]])
+
+    return stars, qsos
 
 if __name__ == '__main__':
+    # JAB Get input for directory to save figure to
+    parser = argparse.ArgumentParser(
+        'Make figures and save them to a particular directory')
+    parser.add_argument('directory', help='The directory to save plots to')
+    args = parser.parse_args()
+    plot_dir = args.directory
+
     # JAB Problem 1
     # JAB Read in star and qso files and make one list of ra and dec
     filepath = '/d/scratch/ASTR5160/week10/'
@@ -82,9 +131,28 @@ if __name__ == '__main__':
     ax.scatter(gz, rw1, s=0.9, color='forestgreen')
     ax.set_xlabel('g - z (mag)')
     ax.set_ylabel('r - W1 (mag)')
-    plt.savefig('testplot.png')
+    
+    # JAB Insert line dividing stars and quasars
+    testline = starqso_line(gz)
+    ax.plot(gz, testline)
+
+    plt.savefig('{}/18testplot.png'.format(plot_dir))
 
     # JAB Determine whether objects are stars or quasars
+    star_cuts, qso_cuts = classify_objs(gz, rw1)
+    
+    # JAB Replot with stars and quasars 
+    fig1 = plt.figure()
+    ax1 = fig1.add_subplot(111)
+    ax1.scatter(star_cuts[0], star_cuts[1], s=0.9, color='forestgreen', label='stars')
+    ax1.scatter(qso_cuts[0], qso_cuts[1], s=0.9, color='purple', label='quasars')
+    ax1.plot(gz, testline, color='k')
+    ax1.set_xlabel('g - z (mag)')
+    ax1.set_ylabel('r - W1 (mag)')
+    ax1.legend()
+
+    plt.savefig('{}/18finalplot.png'.format(plot_dir))
+    
 
 
 
