e2896a24aa6bcf81e68baa760a8d9060e9940094
Jordan Bartlett <jbartle9@uwyo.edu>
2022-03-31 11:13:34 -0600

    new file

---
+++ tasks/week11/class20.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.table import Table, vstack
+from astropy.coordinates import SkyCoord
+import astropy.units as u
+from tasks.week8.class16 import coords_from_sweep, sweep_files
+from tasks.week10.class18 import 
