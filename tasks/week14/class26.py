390e564288b5e90d8781a0c5fd13c3e44d69538b
Jordan Bartlett <jbartle9@uwyo.edu>
2022-04-22 12:50:32 -0600

    read in data set

--- tasks/week14/class26.py
+++ tasks/week14/class26.py
@@ -1,3 +1,15 @@
 import numpy as np
 from astropy.table import Table
 
+if __name__ == '__main__':
+    # JAB Problem 1
+    # JAB Read in file and find mean and variance of each bin
+    filepath = '/d/scratch/ASTR5160/week13/line.data'
+    data = Table.read(filepath, format='ascii')
+    
+    x = np.linspace(0.5, 9.5, 10)
+    mean = np.array([np.mean(data['col{}'.format(i)]) for i in range(1,11)])
+    var = np.array([np.var(data['col{}'.format(i)], ddof=1) for i in range(1,11)])
+
+    # Problem 2
+    
