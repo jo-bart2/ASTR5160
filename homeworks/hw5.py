bf3c84c4e1226f40d1d45e8e3eefebfb34a3e6ee
Jordan Bartlett <jbartle9@uwyo.edu>
2022-04-28 15:36:34 -0600

    working on function

--- homeworks/hw5.py
+++ homeworks/hw5.py
@@ -1,6 +1,20 @@
 import numpy as np
 from astropy.table import Table
 
+# JAB Function to identify quasars in a Table
+def splendid_function():
+    '''
+    Parameters
+    ----------
+    datatable: :class: 'astropy Table'
+       A Table of objects with same columns as sweep files
+
+    Returns
+    -------
+    ----: :class: '~numpy.ndarray'
+       A Boolean array showing which objects in datatable are qsos
+    '''
+
 
 if __name__ == '__main__':
     filepath = '/d/scratch/ASTR5160/week10/qsos-ra180-dec30-rad3.fits'
