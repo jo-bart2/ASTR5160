fc4b73849f6e286c9bbad84616e992a5294fe1ef
Jordan Bartlett <jbartle9@uwyo.edu>
2022-04-29 13:28:39 -0600

    working on training identifier

--- homeworks/hw5.py
+++ homeworks/hw5.py
@@ -1,8 +1,11 @@
 import numpy as np
 from astropy.table import Table
+from astropy.coordinates import SkyCoord
+import astropy.units as u
+
 
 # JAB Function to identify quasars in a Table
-def splendid_function():
+def splendid_function(datatable):
     '''
     Parameters
     ----------
@@ -14,10 +17,10 @@ def splendid_function():
     ----: :class: '~numpy.ndarray'
        A Boolean array showing which objects in datatable are qsos
     '''
+    # JAB Determine the 'known' stars and quasars
+    qsopath = '/d/scratch/ASTR5160/week10/qsos-ra180-dec30-rad3.fits'
+    qsos = Table.read(qsopath)
 
+    
 
 if __name__ == '__main__':
-    filepath = '/d/scratch/ASTR5160/week10/qsos-ra180-dec30-rad3.fits'
-    data = Table.read(filepath)
-
-    print(len(data[0]))
