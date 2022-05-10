2ba6508f23d24269358b4eedef10b3f6ae5e0790
Jordan Bartlett <jbartle9@uwyo.edu>
2022-05-10 14:41:13 -0600

    read in and find mean and variance

--- final/final.py
+++ final/final.py
@@ -1,1 +1,17 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import emcee
+import corner
+from astropy.table import Table
+
+if __name__ == '__main__':
+    # JAB Read in data file and find mean and variance
+    filepath = '/d/scratch/ASTR5160/final/dataxy.fits'
+    data = Table.read(filepath)
+
+    x, y, yerr = data['x'], data['y'], data['yerr']
+    mean = np.mean(y)
+    var = np.var(y, ddof=1)
+
+    
+    
